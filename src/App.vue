e153d2e843be6d87d34034f6694a8244debc487e
sebgro98 <sebgro@kth.se>
2024-01-30 11:30:41 +0100

    Fixed so it wont get error

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HomeView from "@/views/HomeView.vue";
 </script>
 
 <template>
@@ -8,12 +7,12 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HomeView msg="You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+
   </main>
 </template>
 
